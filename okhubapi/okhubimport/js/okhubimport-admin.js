2cd9aa990968ef1dfbfc3995985ef21a83afb1dd
Joe McPherson <j.m.p.mcpherson@gmail.com>
2015-06-01 11:19:31 +0100

    added language filter javascript functionality to theme selector

--- okhubapi/okhubimport/js/okhubimport-admin.js
+++ okhubapi/okhubimport/js/okhubimport-admin.js
@@ -7,24 +7,60 @@
 (function ($) {
 	Drupal.behaviors.okhubimportAdmin = {};
 	Drupal.behaviors.okhubimportAdmin.attach = function(context) {
-		var themeOptionsOriginalArr = new Array();
+		Drupal.okhubimport.themeOptionsOriginal = new Array();
+		var cnt = 0;
 		$("#edit-okhubimport-default-themes option").each(function(){
-			themeOptionsOriginalArr[$(this).val()] = $(this).text();
+			Drupal.okhubimport.themeOptionsOriginal[cnt] = {value:$(this).val(), text:$(this).text()};
+			cnt++;
 		});
-		Drupal.okhubimport.themeOptionsOriginal = themeOptionsOriginalArr;
+		Drupal.okhubimport.themeOptionsOriginal.sort(okhubimportStrSort);
 		$("#edit-okhubimport-default-lang-themes").change(function(){
+			var langSelected = $(this).val();
 			$("#edit-okhubimport-default-themes").empty();//To reset themes
-			$.each(themeOptionsOriginalArr, function(key, value) {   
-			     $("#edit-okhubimport-default-themes")
-			         .append($("<option></option>")
-			         .attr("value",key)
-			         .text(value)); 
+			$.each(Drupal.okhubimport.themeOptionsOriginal, function(key, object) {   
+				var newTextValue = object.text;
+				if(langSelected != 'all'){
+					newTextValue = okhubimportStrForSelectedLang(object.text, langSelected);
+				}
+				if(newTextValue){
+				     $("#edit-okhubimport-default-themes")
+				         .append($("<option></option>")
+				         .attr("value",object.value)
+				         .text(newTextValue));
+				}
 			});
 		});
+		function okhubimportStrSort(a, b) {
+		  if (a.text < b.text)
+			    return -1;
+			  if (a.text > b.text)
+			    return 1;
+			  return 0;
+		}
+		function okhubimportStrForSelectedLang(str, lang){
+			returnStr = '';
+			if (str.indexOf("["+lang+"]") >= 0){
+				/* if we have our choosen language in the text string then we want it, but need to check mutiples with same ID */
+				if(str.indexOf("|") >= 0){
+					/* if we have multiple theme names for this ID split and only get the lang we want */
+					nameArr = str.split("|");
+					for (i = 0, len = nameArr.length, text = ""; i < len; i++) { 
+						/* if we have our choosen language in this elements text string then we want it */
+						if (nameArr[i].indexOf("["+lang+"]") >= 0){
+							if(returnStr){
+								returnStr += ' | ';
+							}
+							returnStr += nameArr[i];
+						}
+					}
+				} else {
+					returnStr = str;
+				}
+			}
+			return returnStr;
+		}
 	}
 	
 	Drupal.okhubimport = Drupal.okhubimport || {};
 	
-
-
 })(jQuery);
